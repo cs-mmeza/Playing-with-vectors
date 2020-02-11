698574a29b7950c3ae075871944501cc84b22837
Mauricio Meza <mmeza1701@gmail.com>
2020-02-11 23:01:53 +0000

    adding comments

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(){
 
   //3D Vector "Va"
   vector <vector<int> > Va;
-  vector <int> Vq;
+  vector <int> Vq; // vector to store queries
   int Vn, q, qElement, qn; 
   int Vsize = 0, Velement = 0;
   //Define Number of Vectors
@@ -14,26 +14,23 @@ int main(){
   cin >> q;
 
   for(int i = 0; i < Vn; i++){
-    //nested Vector
+    //Nested Vector "temp"
     vector<int> temp;
     cin >> Vsize; // Define the size of the vector
   
     for(int j = 0; j < Vsize; j++){
-      cin >> Velement; //add elements to the vector
+      cin >> Velement; //Add elements to the vector
       temp.push_back(Velement);
     }
     Va.push_back(temp);
   }
-
+  //Adding to a new vector to display the queries.
   for(int k = 0; k < q; k++){
     cin >> qn >> qElement;
     Vq.push_back(Va[qn][qElement]);
   }
-  //print the vector
+  //Print the queries.
   for(unsigned int k = 0; k < q; k++){
     cout << Vq[k] << endl;
   }
-
-
-
 }
